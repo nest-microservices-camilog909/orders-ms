782682a15e17c39644b356390e82009763d2a4c8
Camilo Galindo R <cgalindorivera@gmail.com>
2024-08-07 21:34:18 -0500

    envs

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Injectable()
+export class OrdersService {
+	findAll() {
+		return `This action returns all orders`;
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} order`;
+	}
+
+	create(createOrderDto: CreateOrderDto) {
+		return 'This action adds a new order';
+	}
+
+	changeStatus(id: number) {
+		return `This action removes a #${id} order`;
+	}
+}
